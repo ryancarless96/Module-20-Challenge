0529cbe18fe4701e7c4634cacf621b0c42300358
Ryan Carless <ryancarless96@gmail.com>
2023-03-02 16:03:21 -0500

    changed Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-function ProfileForm(props) {
+function Profile(props) {
     const [input, setInput] = useState('');
     let[eagerness, setEagerness] = useState('');
 
@@ -12,7 +12,45 @@ function ProfileForm(props) {
         if(!eagerness) {
             eagerness = 'low;'
         }
-    }
+
+        props.onSubmit({
+            id: Math.random(Math.floor() * 1000),
+            text: input,
+            eagerness: eagerness,
+        });
+
+        setInput('');
+        setEagerness('');
+    };
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    };
+
+    return !props.edit ? (
+        <div>
+            <form className='profile' onSubmit={handleSubmit}>
+                <input
+                type="text"
+                placeholder="Write down your profile"
+                value={input}
+                name="text"
+                className="profile-input"
+                onChange={handleChange}
+                ></input>
+                <div className="dropdown">
+                    <button className={`dropbtn ${eagerness}`}>
+                        {eagerness || 'Priority'}
+                    </button>
+                    <div className='dropdown-content'>
+                        <p onClick={()=> setEagerness(eagernessLevel[0])}>1</p>
+                        <p onClick={()=> setEagerness(eagernessLevel[1])}>2</p>
+                        <p onClick={()=> setEagerness(eagernessLevel[2])}>3</p>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Profile;
