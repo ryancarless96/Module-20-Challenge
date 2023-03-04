49d9c75f6553de6e1ac91a256111a655127993fa
Ryan Carless <ryancarless96@gmail.com>
2023-03-03 20:47:06 -0500

    added to Contact folder

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,36 @@
 import React, {useState} from 'react';
 
+import{validateEmail} from "../../utils/main";
+
+
+function Contact(){
+    const[formState, setFormState] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const[errorMessage, setErrorMessage] = useState('');
+    const {name, email, message} = formState;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(!errorMessage) {
+            console.log('Submit Form', formState);
+        }
+    };
+
+    const handleChange = (e) => {
+        if (e.target.name === 'email') {
+            if(!isValid) {
+                setErrorMessage("Your email doesn't work");
+            } else {
+                setErrorMessage
+            }
+        }
+    }
+}
+
+
 
 export default Contact;
