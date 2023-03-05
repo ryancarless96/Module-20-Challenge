c4e0df37131c51fcb576589895cf86dfc8de5212
Ryan Carless <ryancarless96@gmail.com>
2023-03-05 14:49:01 -0500

    made changes to both App and index

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <title>Ryan Carless's Coding Portfolio</title>
+      <li> <a href="#aboutme" id="about"> About Me</a>
+        </li>
+        <li> <a href="#projects" id="project"> Projects </a>
+        </li>
+        <li> <a href="#contactme" id="contact"> Contact </a>
+        </li> 
     </div>
   );
 }
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hello React!</h1>
+const myFirstElement = <h1>Ryan Carless's Coding Portfolio</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myFirstElement);
+
