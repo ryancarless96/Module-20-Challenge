6a07ed0c3685bc2ff7b775b6e9f49a65c8fd02eb
Ryan Carless <ryancarless96@gmail.com>
2023-03-06 16:25:52 -0500

    added Nav

--- my-react-app/src/components/Header/header.js
+++ my-react-app/src/components/Header/header.js
@@ -1,12 +1,13 @@
 import React from 'react';
-;
+import coverImage from "../../assets(images)/cover-Page";
 
 function Header(props) {
 
   return (
     <header className="flex-row space-between px-1">
       <h1>Ryan Carless</h1>
-
+    <img src={coverImage} alt="gray=background"></img>
+    {props.children}
     </header>
   );
 }
