02eab9b298e1f86b263aeedc701fcf2655dc9168
Ryan Carless <ryancarless96@gmail.com>
2023-03-12 14:59:04 -0400

    added changes to src

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
+
 const myFirstElement = <h1>Ryan Carless's Coding Portfolio</h1>
 
 // ReactDOM.render(myFirstElement, document.getElementById('root'));
