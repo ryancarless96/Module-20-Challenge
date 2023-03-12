660f83d34f61946a2a0649a6cf26f4eafb75c736
Ryan Carless <ryancarless96@gmail.com>
2023-03-12 12:17:42 -0400

    changed index.js

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+
 const myFirstElement = <h1>Ryan Carless's Coding Portfolio</h1>
 
+// ReactDOM.render(myFirstElement, document.getElementById('root'));
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(<App></App>);
 
